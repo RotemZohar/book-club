1e64d827ff1e79a17c0251fe16906d996853e3d8
rzrotem <rzrotem@gmail.com>
2024-12-24 21:26:38 +0200

    please

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,13 +2,9 @@ import cors from "cors";
 import "dotenv/config";
 import http from "http";
 import express from "express";
-import { serve, setup } from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
 import path from "path";
 import { connectToDB } from "./db";
 import mainRouter from "./routes/main-router";
-import swaggerDefinition from "../swagger-definition.json";
-import { notificateTasks } from "./services/task-notifications-service";
 
 const IS_DEV = process.env.NODE_ENV === "development";
 
@@ -25,11 +21,6 @@ const main = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(express.static(path.resolve(__dirname, "public")));
-
-  const specs = swaggerJsDoc(swaggerDefinition);
-  app.use("/api-docs", serve, setup(specs));
-
-  notificateTasks();
 
   app.use("/api", mainRouter);
 
